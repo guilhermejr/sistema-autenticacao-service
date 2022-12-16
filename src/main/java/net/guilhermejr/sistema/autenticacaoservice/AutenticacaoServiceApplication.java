1458cb8517c78239a0ffd7e0924e9f91c7104b4c
Guilherme Jr <falecom@guilhermejr.net>
2022-12-15 21:11:28 -0300

    fix: configuração correta do eureka

--- src/main/java/net/guilhermejr/sistema/autenticacaoservice/AutenticacaoServiceApplication.java
+++ src/main/java/net/guilhermejr/sistema/autenticacaoservice/AutenticacaoServiceApplication.java
@@ -2,8 +2,10 @@ package net.guilhermejr.sistema.autenticacaoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AutenticacaoServiceApplication {
 
 	public static void main(String[] args) {
