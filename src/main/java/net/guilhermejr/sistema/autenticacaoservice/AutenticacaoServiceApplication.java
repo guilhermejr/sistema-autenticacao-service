83c9da19e0a929c5d5d784d7fe7bc2486d486a6e
Guilherme Jr <falecom@guilhermejr.net>
2025-09-13 16:21:49 -0300

    feat: atualizado versão do Spring Boot

--- src/main/java/net/guilhermejr/sistema/autenticacaoservice/AutenticacaoServiceApplication.java
+++ src/main/java/net/guilhermejr/sistema/autenticacaoservice/AutenticacaoServiceApplication.java
@@ -3,10 +3,11 @@ package net.guilhermejr.sistema.autenticacaoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AutenticacaoServiceApplication {
 
 	public static void main(String[] args) {
