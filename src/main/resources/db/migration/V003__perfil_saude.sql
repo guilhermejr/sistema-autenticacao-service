a281edea000e68c230faa71645b02c8c6f8bca88
Guilherme Jr. <falecom@guilhermejr.net>
2026-06-07 17:34:57 -0300

    feat: adicionado o perfil de saúde

---
+++ src/main/resources/db/migration/V003__perfil_saude.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO public.perfis VALUES ('7fec4b9d-a575-4166-a69c-b46aeff4d1e0', 'ROLE_SAUDE');
+INSERT INTO public.usuarios_perfis (usuario_id, perfis_id) VALUES('70d740b7-e632-4c95-b0a7-d824e7aacb4b', '7fec4b9d-a575-4166-a69c-b46aeff4d1e0');
